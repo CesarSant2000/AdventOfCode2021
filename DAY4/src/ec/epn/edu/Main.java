3617bdb7fb606cb31aa055462dfebce314e37628
Cesar J. Santacruz <cesarjsantacruz2000@gmail.com>
2021-12-05 13:28:53 -0500

    DAY4 AFTER

--- DAY4/src/ec/epn/edu/Main.java
+++ DAY4/src/ec/epn/edu/Main.java
@@ -1,15 +1,20 @@
 package ec.epn.edu;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         InputToArray inputToArray = new InputToArray();
         Part1 part1 = new Part1();
         Part2 part2 = new Part2();
-        ArrayList<String> importStr = inputToArray.getTheArray();
-        part1.runPart1(importStr);
-        part2.runPart2(importStr);
+        FirstLine fline = new FirstLine();
+        ArrayList<Integer> firstLine =  fline.getFirstLine();
+        ArrayList<ArrayList<ArrayList<Integer>>> importStr = inputToArray.getTheArray();
+        //System.out.println(firstLine);
+        //System.out.println(importStr);
+        //part1.runPart1(importStr, firstLine);
+        part2.runPart2(importStr, firstLine);
     }
 }
